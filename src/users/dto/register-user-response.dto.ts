4efa9ff1d74d167d53a7ccf123955036a337a04a
Scorhenao <samcorhenao@gmail.com>
2025-01-17 15:53:52 -0500

    feat: add the example of the response in the register

--- src/users/dto/register-user-response.dto.ts
+++ src/users/dto/register-user-response.dto.ts
@@ -1,12 +1,41 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterUserResponseDto {
-  constructor(
-    public status: number,
-    public data: {
-      id: string;
-      name: string;
-      email: string;
-      cellphone: string;
+  @ApiProperty({
+    description: 'HTTP code for registration',
+    example: 201,
+  })
+  status: number;
+
+  @ApiProperty({
+    description: 'User data',
+    example: {
+      id: '12345',
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      cellphone: '+1234567890',
     },
-    public message: string,
-  ) {}
+  })
+  data: {
+    id: string;
+    name: string;
+    email: string;
+    cellphone: string;
+  };
+
+  @ApiProperty({
+    description: 'Message',
+    example: 'User registered successfully',
+  })
+  message: string;
+
+  constructor(
+    status: number,
+    data: { id: string; name: string; email: string; cellphone: string },
+    message: string,
+  ) {
+    this.status = status;
+    this.data = data;
+    this.message = message;
+  }
 }
